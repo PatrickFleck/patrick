22d693cbb4337a261f9dea8681934968256933ef
PatrickFleck <fleckp30@gmail.com>
2026-02-04 08:43:28 +0000

    Add Calculator1

---
+++ courses/coding-in-C/Lab_3/MySolution/Calculator1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    float num1, num2, result;
+    char operation;
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("Enter a second number: ");
+    scanf("%f", &num2);
+    printf("Enter an operation (+, -, *, /): ");
+    scanf(" %c", &operation);
+
+    if (operation == '+') 
+
+    
+
+    
+    }
+
+    
